630487121f73ba360219d1952ab724082f4353ae
emanuele sacchetti <manusacco92@gmail.com>
2023-05-11 18:16:20 +0200

    migliorato il footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,19 @@
 <footer>
-    ciao footer
+    <div class="container">
+        <div class="row">
+            <div class="col-6 text-warning py-5">
+                WatchNow è un marchio registrato. Pappappero
+            </div>
+            <div class="col-6 text-warning py-5">
+                <span>Seguici sui nostri social</span>
+                <span class="px-5">
+                    <a href="#"><img src="img/footer-facebook.png" alt=""></a>
+                    <a href="#"><img src="img/footer-periscope.png" alt=""></a>
+                    <a href="#"><img src="img/footer-pinterest.png" alt=""></a>
+                    <a href="#"><img src="img/footer-twitter.png" alt=""></a>
+                    <a href="#"><img src="img/footer-youtube.png" alt=""></a>
+                </span>
+            </div>
+        </div>
+    </div>
 </footer>
