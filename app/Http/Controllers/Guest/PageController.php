10f3311c96d4b15dbe86727c3c2e41cab831cb25
emanuele sacchetti <manusacco92@gmail.com>
2023-05-11 16:06:23 +0200

    compleatato da punto 1 a 6

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -10,9 +10,9 @@ class PageController extends Controller
 
     public function index(){
 
-        
+        $movies = Movie::all();
 
-        return view('home');
+        return view('home', compact('movies'));
     }
 
 }
